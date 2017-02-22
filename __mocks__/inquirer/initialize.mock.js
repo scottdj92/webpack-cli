b2c8ef05c073bd62b9458f0cd7abedf0f81edcd8
Even Stensberg <evenstensberg@gmail.com>
2017-02-22 20:06:42 +0100

    hotfix: simplify validate-addons logic & npm-exists
    Simplifies the usage of validate-addons and replaces the manual mock of
    nom-exists with the actual.

--- __mocks__/inquirer/initialize.mock.js
+++ __mocks__/inquirer/initialize.mock.js
@@ -1,26 +1,13 @@
 /* eslint node/no-unsupported-features: 0 */
 'use strict';
 const Rx = require('rx');
-const got = require('got');
 const questions = require('../../lib/utils/initial-questions');
+const exists = require('../../lib/utils/npm-exists');
+const initialConfig = require('../../lib/utils/initial-config');
+
 //eslint-disable-next-line
 const prompt = require('prompt.mock');
-const initialConfig = jest.genMockFromModule('../../lib/utils/initial-config');
 
-
-function exists(pkg) {
-	const hostname = 'https://www.npmjs.org';
-	const pkgUrl = `${hostname}/package/${pkg}`;
-	return got(pkgUrl, {method: 'HEAD'})
-			.then( () => {
-				return true;
-			})
-			.catch(() => {
-				return false;
-			});
-}
-
-//
 async function validateAddons(addon) {
 	let arr = [];
 	for(let k of addon) {
@@ -40,7 +27,6 @@ function init(pkg, answer) {
 
 
 module.exports = {
-	exists,
 	validateAddons,
 	init
 };
